2c1489c0314f1b1f9dc27ea0bf0a4def2d73741c
Douglas Rose <douglahr@gmail.com>
2016-02-22 19:32:04 +0100

    WIP User Story #3 - signing up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,17 @@
 require 'data_mapper'
+require_relative '../data_mapper_setup'
 
 class User
    include DataMapper::Resource
 
    property :id, Serial
-   property :first_name, String
+   property :first_name, String, required: true
+   property :second_name, String, required: true
+   property :email, String, required: true, unique: true
+   property :password_digest, Text, required: true
 
+def password=(password)
+self.password_digest=BCrypt::Password.create(password)
+end
 
 end
