71456bbc78d576a8710217126f7331bf7faf934a
Wim Hekkelman <j.hekkelman4@upcmail.nl>
2016-02-22 18:03:17 +0100

    added form to add a space

---
+++ spec/features/spaces_spec.rb
@@ -0,0 +1,15 @@
+feature 'user can list a space' do
+  scenario 'users can fill in a form to register a space' do
+    visit '/'
+    click_button('list a space')
+    fill_in 'name', with: 'A nice place'
+    fill_in 'description', with: 'very big lots of space'
+    fill_in 'price', with: 20
+    fill_in 'address', with: '13 London street'
+    click_button('Submit')
+    expect('page').to have_content('A nice place')
+    expect('page').to have_content('very big lots of space')
+    expect('page').to have_content(20)
+    expect('page').to have_content('13 London street')
+  end
+end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -3,9 +3,7 @@ require_relative '../app/models/user'
 describe User do
   subject(:user){described_class.new}
 
-  it "does something" do
-    expect(user.something).to eq ("hello")
-  end
+
 
 
 
