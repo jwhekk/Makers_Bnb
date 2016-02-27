f9c4ea23bfe86a8dfadb7cca35b8e50e5fd6c797
Wim Hekkelman <j.hekkelman4@upcmail.nl>
2016-02-27 10:57:56 +0100

    added calendar files

---
+++ spec/calendar_controller_spec.rb
@@ -0,0 +1,46 @@
+require_relative '../app/calendar.rb'
+require_relative '../app/models/space_calendar'
+include Calendar_helpers
+
+describe 'calendar controller' do
+
+  let(:start_date) { "2016-03-10"}
+  let(:end_date) { "2016-03-24"}
+  let(:date) { "2016-02-01"}
+
+  let!(:space_calendar) do
+    create_calendar
+  end
+
+  it 'is created all available by default' do
+    expect(space_calendar.availability[134]).to eq 'b'
+  end
+
+  it { is_expected.to respond_to :update_calendar }
+
+  xit 'change status to required when a request is made' do
+    space_calendar.update_calendar(start_date,end_date)
+    expect( space_calendar.availability[75]).to eq 'c'
+  end
+
+  it { is_expected.to respond_to :prepare_calendar }
+
+  it 'returns the array with the year_day2' do
+    array = space_calendar.prepare_calendar(date)
+    expect(array[0]).to eq '2016'
+  end
+
+  it 'returns the array with the month' do
+    array = space_calendar.prepare_calendar(date)
+    expect(array[1]).to eq 'February'
+  end
+
+  it 'returns the array with the days' do
+    array = space_calendar.prepare_calendar(date)
+    expect(array[2][0]).to eq [0,'z']
+    expect(array[2][1]).to eq [1,'b']
+    expect(array[2][11]).to eq [11,'b']
+    expect(array[2][29]).to eq [29,'b']
+    expect(array[2][30]).to eq [0,'z']
+  end
+end
